9570dcd597eeca02efe02180283453f7bd3f6130
mahaprasadnayak <m.p.nayak2000@gmail.com>
2022-07-21 22:09:35 +0530

    new commit

--- main.go
+++ main.go
@@ -13,11 +13,6 @@ import (
 
 var (
 PORT = ":8080"
-// topic      *pubsub.Topic
-// my_project = "<gcp_project name>"
-// my_topic   = "<gcp pubsub topic>"
-// ctx = context.Background()
-// Client *pubsub.Client
 )
 
 func main() {
@@ -26,29 +21,13 @@ func main() {
 	http.ListenAndServe(PORT, nil)
 }
 
-// type message struct {
-// 	Key string            `json:"key"`
-// 	Msg After `json:"message"`
-// }
+
 type Data struct {
 	AfterData After `json:"after"`
 	Key       []int `json:"key"`
   }
   
 type After struct {
-	// Id               int64 `json:"id"`
-	// PreviousBalance  float64 `json:"previousbalance"`
-	// Amount           float64 `json:"amount"`
-	// CurrentBalance   float64 `json:"currentbalance"`
-	// WalletId         int64 `json:"walletid"`
-	// Status           string  `json:"status"`
-	// Type             int64 `json:"type"`
-	// ReversalId       int64 `json:"reversalid"`
-	// StatusCode      int64 `json:"status_code"`
-	// Txnid            int64 `json:"txnid"`
-	// CreatedDate      string `json:"createddate"`
-	// UpdatedDate      string `json:"updateddate"`
-	// TransactionType string `json:"transaction_type"`
 	Id	int64 `json:"id"`
 	Balance float64 `json:"balance"`
 	HoldBalance float64 `json:"hold_balance"`
@@ -61,8 +40,7 @@ func changedDataCapture(w http.ResponseWriter, r *http.Request) {
     fmt.Println("Coming From Changefeed")
     reqBody, _ := ioutil.ReadAll(r.Body)
     fmt.Println("Data coming from Changefeed ",string(reqBody))
-	// createclient_pubsub()
-    // defer Client.Close()
+	
 	digit := ndjson.NewReader(strings.NewReader(string(reqBody)))
     for digit.Next() {
       var data Data
@@ -76,43 +54,13 @@ func changedDataCapture(w http.ResponseWriter, r *http.Request) {
       fmt.Println("Balance",data.AfterData.Balance)
       
 
-	// creating pubsub client
-        
-	// m := message{
-	// 	Key: "wallet1",
-	// 	Msg: data.AfterData,
-	// }
-       
-	// jsonData, err := json.Marshal(m)
-	// if err != nil {
-	// fmt.Println(err)
-	// }
-	// topic = Client.Topic(my_topic)
-	// defer topic.Stop()
-	// topic.PublishSettings.NumGoroutines = 2
-	// result := topic.Publish(ctx, &pubsub.Message{Data: jsonData})
-	// id, err := result.Get(ctx)
-	// if err != nil {
-	// 	fmt.Println("Error in Publish: ", err)
-	// }
-    // fmt.Println("Published a message with msg ID:", id,"txnid::",data.AfterData.Txnid)
+	
 } 
 }
 
  
 
-// func createclient_pubsub() {
-// 	var err error
-// 	Client, err = pubsub.NewClient(ctx, my_project)
 
-// 	if err != nil {
-// 		fmt.Println("Errror in Pub/Sub client creation globally :", err)
-// 	} else {
-// 		topic = Client.Topic(my_topic)
-// 		fmt.Println("Pubsub client created successfullly and Client!,  and topic :", topic)
-// 	}
-
-// }
 
 
  
